866d747a41e12566d231b37770afde891691bb7a
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2018-11-02 16:58:59 -0500

    Update Homework4.java

--- Homework4.java
+++ Homework4.java
@@ -30,13 +30,33 @@ public class Homework4 {
 		}
 	}
 
-	private static void dfs(int[][] matrix, char[] vertices, int index, Set<Character> alreadyVisited) {
+	private static void dfs(int[][] matrix, char[] vertices, int index, boolean[] alreadyVisited) {
 		System.out.print(vertices[index]);
-		alreadyVisited.add(vertices[index]);
+		alreadyVisited[index] = true;
 		for (int i = 0; i < matrix[index].length; i++) {
-			if (matrix[index][i] == 1 && !alreadyVisited.contains(vertices[i])) {
+			if (matrix[index][i] == 1 && alreadyVisited[i] == false) {
 				System.out.print(" -> ");
 				dfs(matrix, vertices, i, alreadyVisited);
+			}
+		}
+	}
+
+	private static void bfs(int[][] matrix, char[] vertices, int index) {
+		Queue<Integer> queue = new LinkedList<>();
+		boolean[] alreadyVisited = new boolean[matrix.length];
+		System.out.print(vertices[index]);
+		alreadyVisited[index] = true;
+		queue.add(index);
+		while (!queue.isEmpty()) {
+			int now = queue.poll();
+			if (alreadyVisited[now] == false) {
+				System.out.print(" -> " + vertices[now]);
+				alreadyVisited[now] = true;
+			}
+			for (int i = 0; i < matrix[now].length; i++) {
+				if (matrix[now][i] == 1 && alreadyVisited[i] == false) {
+					queue.add(i);
+				}
 			}
 		}
 	}
@@ -48,10 +68,14 @@ public class Homework4 {
 				{ 1, 0, 0, 0, 0, 0, 0, 1, 1, 0 }, { 0, 1, 0, 0, 0, 0, 0, 0, 1, 1 }, { 0, 0, 1, 0, 0, 1, 0, 0, 0, 1 },
 				{ 0, 0, 0, 1, 0, 1, 1, 0, 0, 0 }, { 0, 0, 0, 0, 1, 0, 1, 1, 0, 0 } };
 
+		System.out.println("The adjacency matrix of the graph is:");
 		printMatrix(graph);
 
 		System.out.println('\n' + "The start vertex of dfs is:" + vertices[0]);
-		dfs(graph, vertices, 0, new HashSet<>());
+		dfs(graph, vertices, 0, new boolean[graph.length]);
+
+		System.out.println("\n\n" + "The start vertex of bfs is:" + vertices[0]);
+		bfs(graph, vertices, 0);
 	}
 
 }
