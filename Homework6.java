85afff2dcc59fff908cb04b55dc4026176800c71
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2018-11-25 00:50:50 -0600

    Add files via upload

---
+++ Homework6.java
@@ -0,0 +1,25 @@
+//Implement Hash table.
+//
+//Pick 20 random words.  Each word must be of different lengths, maximum length 8 and minimum length 3.
+//
+//The words will be of letters a-zA-Z0-9 and the space character.
+//
+//Insert them into a hash table.  The hashing algorithm uses ASCII table values for each character and adds up the values. Initially the table size is 31.  The program should increase the table size and rehash.
+//
+//The collision resolution is done using quadratic probing.
+//
+//At the end print the total number of collisions you get.
+//
+//If the table size is more than 20,000 and the words to insert are 8000, is the above hashing algorithm adequate ? why ?
+
+class HashTable {
+
+}
+
+public class Homework6 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
