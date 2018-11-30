26b1bd8cd7060edba63f58a1b5d9882fd14e11c6
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2018-11-30 17:39:29 -0600

    Update Homework6.java

--- Homework6.java
+++ Homework6.java
@@ -15,39 +15,37 @@ import java.util.Arrays;
 //
 //If the table size is more than 20,000 and the words to insert are 8000, is the above hashing algorithm adequate ? why ?
 
-class HashTable {
-	int collisions;
-	int tableSize;
-	int load;
+class HashTable {				// establish a class named HashTable
+	int collisions;				// reserve an integer used to store the happenings of collisions
+	int tableSize;				// reserve an integer used to store the size of table
+	int load;					// reserve an integer used to store the load for the calculation of load factor
 	String[] table;
 
-	HashTable() {
+	HashTable() {							// constructor with the default table size set as 31
 		this.table = new String[31];
 		this.tableSize = 31;
 		this.load = 0;
 	}
 
-	HashTable(int i) {
+	HashTable(int i) {						// another constructor where the size could be set manually
 		this.table = new String[i];
 		this.tableSize = i;
 		this.load = 0;
 	}
 
-	void put(String key, String val) {
+	void put(String key, String val) {		// function for insertion
 		if (load * 2 >= tableSize) {
 			reHash();
 		}
 		int hashVal = getHash(key);
 		if (table[hashVal] != null) {
-			collisions++;
 			hashVal = quadratic(hashVal);
 		}
 		table[hashVal] = val;
 		load++;
 	}
 
-	void reHash() {
-		int record = collisions;
+	void reHash() {						// function to increase the size of table and rehash 
 		int nextPrime = generateNextPrime(tableSize);
 		String[] prevTable = table.clone();
 		this.table = new String[nextPrime];
@@ -58,10 +56,9 @@ class HashTable {
 				put(i, i);				
 			}
 		}
-		collisions = record;
 	}
 
-	int getHash(String n) {
+	int getHash(String n) {				// function to calculate the hash value
 		int val = 0;
 		for (int i = 0; i < n.length(); i++) {
 			val += n.charAt(i);
@@ -69,16 +66,17 @@ class HashTable {
 		return val % tableSize;
 	}
 
-	int quadratic(int hash) {
+	int quadratic(int hash) {			// function for quadratic probing
 		int i = 1, next = hash;
 		while (table[next] != null) {
+			collisions++;
 			next = (hash + i * i) % tableSize;
 			i++;
 		}
 		return next;
 	}
 
-	int generateNextPrime(int cur) {
+	int generateNextPrime(int cur) {	// used to generate the next prime number
 		int next = cur * 2;
 		while (!isPrime(next)) {
 			next++;
@@ -86,7 +84,7 @@ class HashTable {
 		return next;
 	}
 
-	boolean isPrime(int i) {
+	boolean isPrime(int i) {			// used to check if the number is a prime
 		for (int j = 2; j < i; j++) {
 			if (i % j == 0)
 				return false;
@@ -97,14 +95,21 @@ class HashTable {
 
 public class Homework6 {
 	public static void main(String[] args) {
-		String[] words = new String[] { "tribunal", "aviator", "draft", "aroma", "draft", "costly", "700 kB", " aKp",
-				"skimpy", "Amd", "WGLpX5i", "pi34EhMJ", "hair", "castic", "running", "screen", "13Y8g", "clinic",
-				"dandruff", "NOJhj8", "ninja" };
+		// generate 20 random words
+		String[] words = new String[] { "tribunal", "aviator", "draft", "aroma", "draft", "costly", "700 kB", "AK 47",
+				"skimpy", "And", "5 dollars", "2PM", "hair", "castic", "running", "screen", "YEARS", "clinic",
+				"guitar", "goodtime", "ninja" };
+		// create a new instance of the HashTable we wrote
 		HashTable table = new HashTable();
+		// insert the 20 random words into the HashTable
 		for (String i : words) {
 			table.put(i, i);
 		}
-		System.out.println("The total number of collisions is: " + table.collisions);		
+		//Print the total number of collisions.
+		System.out.println("After inserting the 20 words, the total number of collisions is: " + table.collisions);		
 	}
-
 }
+
+//If the table size is more than 20,000 and the words to insert are 8000, is the above hashing algorithm adequate ? why ?
+//Answer:
+//	if the table size is more than 20,000, and the words to insert are 8000, the algorithm would not be adequate, because the 
